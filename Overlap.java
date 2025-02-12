0e191d8be08ea6123d7aa23741d6b470d81c574c
Esteban Parra <estebanpr014@gmail.com>
2025-02-12 12:40:11 -0600

    Added Files
    This is coding 101. Not really

--- Overlap.java
+++ Overlap.java
@@ -11,7 +11,16 @@ public class Overlap {
     }
     
     public Double assymetricDice(){
-        return 0.0;
+        return 0.5;
+    }
+
+
+    public Double assymetricJaccard(){
+        return 3.0;
+    }
+    
+    public Double assymetricDice(){
+        return 1.0;
     }
     
 }
--- StopwordRemoval.java
+++ StopwordRemoval.java
@@ -21,13 +21,20 @@ public class StopwordRemoval {
     public LinkedList<String> removeStopwords(LinkedList<String> wordList) {
         LinkedList<String> curatedWordList = new LinkedList<>();
 
-        for (String word : wordList) {
-            if (stopwords.contains(word) == false) {
-                curatedWordList.add(word);
-            }
-        }
-
         return curatedWordList;
     }
+
+// Comenteafafsa
+
+
+    I never imagined this was 
+    possible, but “The Dictionary of Obscure Sorrows” 
+    is now a New York Times bestseller! 
+
+    There are no words for how grateful I feel, 
+    knowing that I’m not alone. If you’re thinking of 
+    gifts for anyone you know who’s creative, thoughtful,
+     or going through confusing times, you might want to 
+     order them a copy now:
 
 }
